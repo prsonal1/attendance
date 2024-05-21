ad9171950a3e5f7216ef3f46262cf53367f1a34d
Pouria <shahvarpour@saf.co.ir>
2024-05-21 10:48:08 +0330

    Insert cleaning function

--- function/txt2csv.py
+++ function/txt2csv.py
@@ -1,10 +1,11 @@
 # Liberary
 import csv
+import pandas as pd
 
 def txt2csv():
     # Define the input and output file paths
     input_file_path = 'data/AGL_001.TXT'
-    output_file_path = 'date.csv'
+    output_file_path = 'data.csv'
 
     # Read the content of the text file
     with open(input_file_path, 'r') as txt_file:
@@ -22,5 +23,20 @@ def txt2csv():
         for line in lines[1:]:
             row = line.strip().split('\t')
             csv_writer.writerow(row)
+    
 
     print(f"Data successfully converted to {output_file_path}")
+    
+    # Read the CSV file
+    df = pd.read_csv('data.csv')
+
+    # Select only the required columns
+    selected_columns = df[['No', 'EnNo', 'DateTime']]
+
+    # Save the selected columns to a new CSV file
+    selected_columns.to_csv('data_customize.csv', index=False)
+
+    print(f"Data successfully cleaning")
+
+
+txt2csv()
